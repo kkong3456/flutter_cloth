ea346bb6f6bf3103067a5809be08b5fdd6c61bc3
kkong7 <kkongk@naver.com>
2022-05-21 16:26:41 +0900

    20220521-1

--- lib/data/weather.dart
+++ lib/data/weather.dart
@@ -1,4 +1,4 @@
-class Weather{
+class Weather {
   String date;
   int time;
   int pop;
@@ -9,9 +9,25 @@ class Weather{
   int tmp;
   int reh;
 
-  Weather({this.date,this.time,this.pop,this.pty,this.pcp,this.sky,this.wsd,this.tmp,this.reh});
+  Weather(
+      {this.date,
+      this.time,
+      this.pop,
+      this.pty,
+      this.pcp,
+      this.sky,
+      this.wsd,
+      this.tmp,
+      this.reh});
 
-  factory Weather.formJson(Map<String,dynamic>data){
-    return Weather
+  factory Weather.formJson(Map<String, dynamic> data) {
+    return Weather(
+      date: data["fcstDate"],
+      time: int.tryParse(data["fcstTime"] ?? "") ?? 0,
+      pop: int.tryParse(data["POP"] ?? "") ?? 0,
+      pty: int.tryParse(data["PTY"] ?? "") ?? 0,
+      sky: int.tryParse(data["SKY"] ?? "") ?? 0,
+      wsd: double.tryParse(data["WSD"] ?? "") ?? 0,
+    );
   }
 }
